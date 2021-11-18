6f2bf50a183b13ee89f7ae961f799a98104349b1
Jeff <jeffprogramacion@gmail.com>
2021-11-18 17:31:34 -0500

    design profile screen

--- App.js
+++ App.js
@@ -1,44 +1,38 @@
 import 'react-native-gesture-handler';
 import React, { useState,useEffect } from 'react';
-import {   SafeAreaView,   ActivityIndicator} from 'react-native'
+import { ActivityIndicator } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
-
+import firebase from './src/lib/firebase'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 /* screen */
 
 import MainStack from './src/screens/mainStack/MainStack';
 import AuthStack from './src/screens/authScreen.js/AuthStack';
-import Colors from './src/res/ColorsLib';
 
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const [loaded] = useFonts({
-    Questrial: require('./src/assets/tipografias/Questrial.ttf'),
-  });
+
 
   useEffect(() => {
- 
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        
+        // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         const uid = user.uid;
         console.log(uid)
-
-        setIsSignedIn(false)
+        setIsSignedIn(true)
         // ...
       } else {
         // User is signed out
         // ...
-
         console.log("no logueado");
         setIsSignedIn(false)
       }
@@ -48,33 +42,35 @@ function App() {
   
 
 
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [loaded] = useFonts({
+    Questrial: require('./src/assets/tipografias/Questrial.ttf'),
+  });
+  
+if (!loaded) {
+return <ActivityIndicator />;
+}
+
   return(
-    <SafeAreaView style={{justifyContent:'center', alignContent: 'center',flex: 1}}>
-          {!loaded  ?  <ActivityIndicator  size="large" color={Colors.green}/> 
-        :
-          <NavigationContainer >
-        
-              <Stack.Navigator  
-                    initialRouteName="Login"
-                    screenOptions={{
-                        headerShown: false
-                      }}
-                >
-                  {isSignedIn ? (
-                    <>  
-                      <Stack.Screen name="MainStack" component={MainStack} />
-                    </>
-                  ) : (
-                    <>
-                    
-                      <Stack.Screen name="AuthStack" component={AuthStack} />
-                    </>
-                  )}
-            </Stack.Navigator>
-          </NavigationContainer>
-        }
-      
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator  
+              initialRouteName="Login"
+              screenOptions={{
+                  headerShown: false
+                }}
+          >
+            {isSignedIn ? (
+              <>  
+                <Stack.Screen name="MainStack" component={MainStack} />
+              </>
+            ) : (
+              <>
+              
+                <Stack.Screen name="AuthStack" component={AuthStack} />
+              </>
+            )}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 
 }
--- src/screens/profileScreen/ProfileScreen.js
+++ src/screens/profileScreen/ProfileScreen.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { View, Text,Button,Alert,NativeModules } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { View, Text,TouchableOpacity ,Alert,Image,StyleSheet } from 'react-native'
 import {  signOut } from "firebase/auth";
 import {auth} from '../../lib/firebase';
+import  Colors  from '../../res/ColorsLib';
 
 
 export default function ProfileScreen({ navigation }){
+  const [currenUser, setCurrenUser] = useState(null)
 
- 
+  useEffect(() => {
+   
+      const user = auth.currentUser;
+      if (user) {
 
-  const user = auth.currentUser;
+        setCurrenUser(user)
+      } else {
+        console.log("No se pudo obtener el usuario")
+      }
 
-if (user) {
-  // User is signed in, see docs for a list of available properties
-  // https://firebase.google.com/docs/reference/js/firebase.User
-  // ...
-  console.log('Usario actual');
-  console.log(user.email)
-} else {
-  console.log("No se pudo obtener el usuario")
-}
+  }, [currenUser])
+
+
 
   const handelsignOut = ()=>{
     signOut(auth).then(() => {
@@ -26,7 +28,6 @@ if (user) {
         "Has cerrado sesión",
         "Vuelve pronto!"
       )
-
     }).catch((error) => {
       Alert.alert(
         "No pudimos cerrar",
@@ -37,13 +38,67 @@ if (user) {
   }
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
-        <Button
-          title="Cerrar Sesión"
-          onPress={handelsignOut}
-        />
+      <View style={styles.container}>
+        <Image style={styles.paper} source={require('../../assets/image/Papel.png')}/>  
+        <Image style={styles.logo} source={require('../../assets/image/logo.png')}/>
+        <Image style={styles.avatar} source={require('../../assets/image/avatar.png')}/>
+        
+        <View>
+          <Text style={styles.email}>
+            {currenUser != null ? currenUser.email : ''}
+          </Text>
+        </View>
+        
+        <TouchableOpacity 
+          onPress={handelsignOut}          
+          style={styles.btn}
+        >
+          <Text style={styles.textBtn}>Cerrar Sesión</Text>
+        </TouchableOpacity> 
       </View>
     );
   }
   
+  const styles = StyleSheet.create({
+    container:{
+      flex: 1, 
+      alignItems: 'center' 
+    },
+    logo:{
+        position: 'absolute',
+        alignSelf:'center',
+        top: 70,
+    },
+    paper:{
+        width: '100%',
+    },
+    avatar:{
+      borderRadius: 100,
+      marginTop:-60,
+      borderColor: 'white',
+      borderWidth:  6, 
+      borderStyle:  'dashed',
+    },
+    email:{
+      fontFamily: 'Questrial',
+      fontSize:20,
+      marginTop: 20,
+      marginBottom:20
+    },
+    btn:{
+      borderRadius: 50,
+      width:'85%',
+      alignSelf: 'center',
+      backgroundColor: Colors.naranja,
+      padding: 10,
+      marginTop: 50,
+      elevation: 2
+    },
+    textBtn:{
+      color: Colors.white,
+      fontFamily: 'Questrial',
+      textAlign: 'center',
+      fontSize:15
+    }
+
+})
