9307b3518dd4fc8d688043af31a878973628fc8c
Jeff <jeffprogramacion@gmail.com>
2021-12-01 09:54:18 -0500

    add list centros de acopio

--- src/res/CentrosDeAcopio.js
+++ src/res/CentrosDeAcopio.js
@@ -1,58 +1,72 @@
 export const centrosAcopio = [
     {
-        nombre: "IKOPORTEX",
+        nombre: "MC RECUPERADORES",
         lat: "4.7074788",
         long: "-74.2392081",
         city: 'Mosquera',
-        url: "https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814"
+        url: "https://goo.gl/maps/fA35hbW5DJKCUjc29"
+    },
+    {
+        nombre: "ECOLIDER DE OCCIDENTE",
+        lat: "4.7073793",
+        long: "-74.2311239",
+        city: 'Mosquera',
+        url: 'https://goo.gl/maps/scT2Pfc4oi5C9qG9A'
+    },
+    {
+        nombre: "BODEGA DE RECICLAJE EL IMÁN",
+        lat: "4.7073793",
+        long: "-74.2311239",
+        city: 'Mosquera',
+        url: 'https://goo.gl/maps/bdctVFGE2Mmj9U9w5'
+    },
+    {
+        nombre: "RECICLAJE DUARTE",
+        lat: "4.7073793",
+        long: "-74.2311239",
+        city: 'Mosquera',
+        url: 'https://goo.gl/maps/91egCZrBf4RirfLt5'
     },
     {
         nombre: "RECICLAIR SAS",
         lat: "4.7073793",
         long: "-74.2311239",
         city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
+        url: 'https://g.page/Reciclairsas?share'
     },
     {
-        nombre: "RECICLAIR SAS",
+        nombre: "ECO RESIDUOS NACIONALES",
         lat: "4.7073793",
         long: "-74.2311239",
         city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
+        url: 'https://goo.gl/maps/EdVc1LwFqbH3M5QN9'
     },
     {
+        nombre: "RECICLAMAS OCCIDENTE",
+        lat: "4.7073793",
+        long: "-74.2311239",
+        city: 'Mosquera',
+        url: 'https://goo.gl/maps/e15zXsWhBADKfJ1YA'
+    },{
         nombre: "IKOPORTEX",
         lat: "4.7073793",
         long: "-74.2311239",
         city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
+        url: 'https://g.page/icoporR2?share'
     },
-    {
-        nombre: "RECICLAIR SAS",
+    ,{
+        nombre: "EKOPLANET MOSQUERA",
         lat: "4.7073793",
         long: "-74.2311239",
         city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
+        url: 'https://goo.gl/maps/8kb3iVADQ5e2rRM27'
     },
-    {
-        nombre: "RECICLAIR SAS",
+    ,{
+        nombre: "ECOCOMPUTO ",
         lat: "4.7073793",
         long: "-74.2311239",
         city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
-    },
-    {
-        nombre: "IKOPORTEX SAS",
-        lat: "4.7073793",
-        long: "-74.2311239",
-        city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
-    },{
-        nombre: "RECICLAIR SAS",
-        lat: "4.7073793",
-        long: "-74.2311239",
-        city: 'Mosquera',
-        url: 'https://www.google.com/maps/dir/4.7202041,-74.2296709/IKOPORTEX,+Villa+Lady,+Cra.+6+No.+13ª+-+25+barrio,+Mosquera,+Cundinamarca/@4.7018304,-74.2108136,13.8z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8e3f9b4d6b42edab:0xec59321be270fafa!2m2!1d-74.1740464!2d4.6948814'
+        url: 'https://goo.gl/maps/7Ja56ev78RUmZNCP8'
     },
 
 ] 
