5072090924255fc8529f6ed9a7bb191e2ac8dbf6
Jeff <jeffprogramacion@gmail.com>
2021-12-10 11:30:47 -0500

    screem compostaje arregaklda de cierre automatico

--- src/screens/categoriesScreens/Compostaje.js
+++ src/screens/categoriesScreens/Compostaje.js
@@ -7,18 +7,18 @@ import FontsVariables from '../../res/FontVariables';
 
 export default function Compostaje() {
 
-  const [playing, setPlaying] = useState(false);
+ /*  const [playing, setPlaying] = useState(false); */
 
-  const onStateChange = useCallback((state) => {
+/*   const onStateChange = useCallback((state) => {
     if (state === "ended") {
       setPlaying(false);
       Alert.alert("¡El video ha terminado de reproducirse!");
     }
-  }, []);
+  }, []); */
 
-  const togglePlaying = useCallback(() => {
+  /* const togglePlaying = useCallback(() => {
     setPlaying((prev) => !prev);
-  }, []);
+  }, []); */
 
     return (
         <ScrollView >
@@ -74,6 +74,34 @@ export default function Compostaje() {
                 {"\n"}{"\n"}
                 -Efectos sobre la salud del suelo.  
                 </Text>
+
+                <Text style={styles.description}>Se trata de un producto natural, sin compuestos químicos y libre de patógenos. En muchos casos actúa como bactericida y fungicida.
+                </Text>
+
+                <Text style={styles.description}>Efectos sobre los nutrientes de las plantas.   </Text>
+
+                <Text style={styles.description}>Al ser un producto rico en nutrientes y macronutrientes, se convierte en un excelente abono para las plantas. 
+                </Text>
+
+                <Text style={styles.description}>
+                Beneficios económicos. 
+                </Text>
+
+                <Text style={styles.description}>
+                No es necesario adquirir este producto, ya que se obtiene de un proceso muy sencillo que se puede realizar en el hogar 
+                </Text>
+                <Text style={styles.title}>Usos del compos :</Text>
+                <Image style={styles.imagen} source={require('../../assets/compostaje/usos.jpg')}/>
+                <Text style={styles.description}>
+                Depende del grado de madurez del compost y del tipo de cultivo para el cual se destina, siendo las épocas óptimas para su aplicación primavera y otoño, épocas en que el suelo está caliente y la adición del compost lo calentará aún más favoreciendo la actividad de los microorganismos y el desarrollo de las raíces de la planta.
+                {"\n"}{"\n"}
+                Diferenciamos dos grados de madurez: 
+                {"\n"}{"\n"}
+                • Compost fresco (2-3 meses compostando): el que ha tenido un período de madurez corto y en el que se aprecia aún material sin descomponer. Se usa principalmente como protección frente ante los cambios de temperatura y de humedad, en especial frente a heladas. Además mejora las características del suelo y evita la aparición de malas hierbas.
+                {"\n"}{"\n"}
+                • Compost maduro (aproximadamente 5-6 meses compostando): aquel que ha tenido un período de madurez largo y no se aprecian materiales sin descomponer (excepto aquellos de muy lenta descomposición, ramas, cáscaras de huevo, etc.). Se diferencia fácilmente por su textura terrosa y su color oscuro. Su uso principalmente es como fertilizante ya que aporta elementos minerales (nitrógeno, fósforo, potasio, etc.), además de favorecer la capacidad de retención de agua.
+                </Text>
+
             </View>
         </ScrollView>
     )
